37602b64bc48cc03a9f30a1fa9449aff6694d01f
Lakisha Adu <132563727+lakishaadu@users.noreply.github.com>
2024-07-10 11:41:33 +0200

    Update animated-title.js

--- animated-title.js
+++ animated-title.js
@@ -1,33 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const originalTitle = document.title;
-    const messages = ["Komm zurück!", "Wir vermissen dich!", "Hier gibt es mehr!"];
+    const messages = ["Komm zurück!", "Wir vermissen dich!"];
     let currentMessage = 0;
-    let currentChar = 0;
-    let title = '';
     let interval;
-
-    function animateTitle() {
-        if (currentChar < messages[currentMessage].length) {
-            title += messages[currentMessage].charAt(currentChar);
-            currentChar++;
-            document.title = title;
-            interval = setTimeout(animateTitle, 200); // Geschwindigkeit der Zeichen
-        } else {
-            currentChar = 0;
-            title = '';
-            currentMessage = (currentMessage + 1) % messages.length;
-            interval = setTimeout(animateTitle, 1000); // Zeit bis zur nächsten Nachricht
-        }
-    }
 
     function startAnimation() {
         if (!interval) {
-            animateTitle();
+            interval = setInterval(function() {
+                document.title = messages[currentMessage];
+                currentMessage = (currentMessage + 1) % messages.length;
+            }, 1000); // Wechsel alle 1 Sekunde
         }
     }
 
     function stopAnimation() {
-        clearTimeout(interval);
+        clearInterval(interval);
         interval = null;
         document.title = originalTitle;
     }
